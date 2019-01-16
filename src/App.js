23a5bcbe00d8b1992d139b13933455cad6c5bfd0
Siddhant <sid.simplybest@gmail.com>
2019-01-16 11:47:37 +0530

    Deleting node modules
    Date-Operations Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import DateOperations from './containers/DateOperations';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <DateOperations/>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import classes from './Input.css';
+
+const input = (props) => {
+    let inputElement = null;
+    let errorBlock = null;
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
+
+    switch (props.elementType) {
+        case ('input'):
+            if (props.errorMsg) {
+                errorBlock = <div className={classes.ErrorTextStyle}>{props.errorMsg}</div>
+            } else {
+                errorBlock = null;
+            }
+            inputElement = <input
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        default:
+            inputElement = <input
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+    }
+    return (
+        <div>
+            <div className={classes.Input}>
+                <label className={classes.Label}>{props.label}</label>
+                {inputElement}
+            </div>
+            {errorBlock}
+        </div>
+
+    );
+
+};
+
+export default input;
